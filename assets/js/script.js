59751e67b020f0893874395b41af889d35e39fce
AarushSah <98967041+AarushSah@users.noreply.github.com>
2022-08-16 21:55:33 -0400

    Removed Template Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
+//const overlay = document.querySelector("[data-overlay]");
 
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
@@ -28,10 +28,12 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
+/*
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
+*/
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
@@ -50,9 +52,10 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
+/*
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
+*/
 
 
 // custom select variables
